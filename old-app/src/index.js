fa0f96c9b44ace5c02f27c2617e0d48a03b21e18
William Stocks <willstocks@me.com>
2024-11-15 15:42:15 +0000

    Add React bootstrap css to index.js

--- old-app/src/index.js
+++ old-app/src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDom from 'react-dom';
+import App from './App';
 
-
-const myFirstElement = <h1>Hello React!</h1>
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+ReactDom.render(<App />, document.getElementById('root'));
